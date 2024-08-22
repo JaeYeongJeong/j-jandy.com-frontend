0729a11a684ae45f857002f3a339a89aac5d0afa
JaeYeong Jeong <94372210+JaeYeongJeong@users.noreply.github.com>
2024-08-23 00:45:39 +0900

    Change api url for apply proxy

--- src/Util/api-url.js
+++ src/Util/api-url.js
@@ -1,7 +1,11 @@
 let apiUrl = 'https://localhost:8080';
 
 if (import.meta.env.VITE_API_URL) {
-  apiUrl = '/api/';
+  apiUrl = '/api';
+
+  if (apiUrl.endsWith('/')) {
+    apiUrl = apiUrl.replace(/\/$/, '');
+  }
 }
 
 export default apiUrl;
